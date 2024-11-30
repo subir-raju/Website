78cc03f098af7b9675d41d3e7adbf238a50fb6d2
subir-raju <subir.raju0@gmail.com>
2024-11-30 03:56:54 +0200

    navbar and hamburger button add

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
+import {HiMenuAlt4, HiX} from 'react-icons/hi';
+import { motion } from 'framer-motion';
 
-import logo1 from '../../assets/logo1.png';
+import logo4 from '../../assets/logo4.png';
+import './Navbar.scss';
 
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false)
   return (
-    <nav>
-        <div>
-            <img src = {logo1} alt ="" />
-        </div>
-        <ul>
-            {['Home', 'About Me', 'Work', 'Skills', 'Contact'].map((item) => (
-                <li key = {`link-${item}`}>
-                    <div />
-                    <a href = {`#${item}`}>{item}</a>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
+        <img src={logo4} alt="" />
+      </div>
+      <ul className="app__navbar-links">
+        {["Home", "About Me", "Work", "Skills", "Contact"].map((item) => (
+          <li className="app__flex p-text" key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+
+      <div className="app__navbar-menu">
+        <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+        {toggle && (
+          <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition={{ duration: 0.85, ease: "easeOut" }}
+          >
+            <HiX onClick={() => setToggle(false)} />
+            <ul>
+              {["home", "about me", "work", "skills", "contact"].map((item) => (
+                <li key={item}>
+                  <div />
+                  <a href={`#${item}`} onClick={() => setToggle(false)}>
+                    {item}
+                  </a>
                 </li>
-            ))}
-        </ul>
+              ))}
+            </ul>
+          </motion.div>
+        )}
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar

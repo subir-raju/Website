a5439a64ac7450c3c7f4da788ec37f736993fb0e
subir-raju <subir.raju0@gmail.com>
2024-12-01 04:06:59 +0200

    header part created

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import pro from "../../assets/center.png";
+import './Header.scss';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section id ="profile">
+      <div className="section__pic-container">
+        <img src={pro} alt="profile pic" />
+      </div>
+      <div className="section__text">
+        <p className="section__text-p1">Hello, I'm</p>
+        <h1 className="title">Subir Dey Raju</h1>
+        <p className="section__text-p2">Data Analyst</p>
+      </div>
+    </section>
   )
 }
 
